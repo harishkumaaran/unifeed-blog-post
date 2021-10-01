fbc4e8301d6900b6f69f930f88ae9c3f8a5cfaf2
harish <harishkumaaran23@gmail.com>
2021-10-01 11:02:58 +0530

    FEAT Animate all buttons

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ function changeBgAgain(obj) {
     obj.parentNode.style.backgroundColor = 'rgba(245, 158, 11)';
     obj.style.color='rgba(17, 24, 39)';
 }
+
+function changeBgButton2(obj){
+    obj.style.transition= 'all 0.2s';
+    obj.style.color='rgb(156, 163, 175)';
+}
+
+function changeBgButton2Again(obj){
+    obj.style.transition= 'all 0.2s';
+    obj.style.color='rgb(55, 65, 81)';
+}
